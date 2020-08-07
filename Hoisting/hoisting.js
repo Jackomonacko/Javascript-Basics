98428880472603d800f026d2deeed090a207d456
Jackomonacko <jacksadowski33@gmail.com>
2020-08-07 12:13:36 -0400

    Create hoisting.js

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,26 @@
+console.log(fruit);
+var fruit = 'mango';
+console.log(fruit);
+
+// console.log(typeOfLobster);
+// let typeOfLobster = 'Hard Shell';
+// console.log(typeOfLobster);
+
+// console.log(typeOfLobster);
+// const typeOfLobster = 'Hard Shell';
+// console.log(typeOfLobster);
+
+bark();
+function bark() {
+	console.log('woof');
+}
+
+// console.log(yell);
+// var yell = function() {
+// 	console.log('ahhhhh');
+// };
+
+// yell();
+// let yell = function() {
+// 	console.log('ahhhhh');
+// };
