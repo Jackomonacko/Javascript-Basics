33a573fa06b0092ba77af8b48bd00223d2720a4a
Jackomonacko <jacksadowski33@gmail.com>
2020-08-08 11:29:11 -0400

    Create array-find.js

---
+++ Array.find/array-find.js
@@ -0,0 +1,29 @@
+let movies = [ 'The Patriot', 'The Bounty', 'Gallipoli', 'Get the Gringo', 'Hacksaw Ridge' ];
+
+const movie = movies.find((movie) => {
+	return movie.includes('The');
+});
+
+const movie2 = movies.find((m) => m.indexOf('The') === 0);
+
+const albums = [
+	{
+		title: 'The Very Best of Grateful Dead',
+		band: [ 'Grateful Dead' ],
+		year: 2003
+	},
+	{
+		title: 'Junta',
+		band: [ 'Phish' ],
+		year: 1989
+	},
+	{
+		title: 'Electri Ladyland',
+		band: [ 'Jimi Hendrix Experience' ],
+		year: 1968
+	}
+];
+
+const after2000 = albums.find((a) => a.year >= 2000);
+
+const jimiAlbum = albums.find((b) => b.band.includes('Jimi Hendrix Experience'));
