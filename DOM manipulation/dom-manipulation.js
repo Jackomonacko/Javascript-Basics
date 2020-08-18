5ccd6d6e61339dbf172b79f3908282dd1445f569
Jackomonacko <jacksadowski33@gmail.com>
2020-08-18 13:52:14 -0400

    Create dom-manipulation.js

---
+++ DOM manipulation/dom-manipulation.js
@@ -0,0 +1,24 @@
+//to run in console
+
+document.getElementById('mountain-photo')
+
+document.getElementById('main')
+
+document.getElementsByTagName('li');
+
+document.getElementsByTagName('h1');
+
+const inputs = document.getElementsByTagName('input');
+
+inputs[0];
+
+document.getElementsByClassName('special');
+
+const specials = document.getElementsByClassName('special');
+for (let el of specials) {
+  console.log(el);
+}
+
+const arr = [...specials];
+
+arr.pop()
