73453d098d15d066799722e2338e6e90cb3160f0
Jackomonacko <jacksadowski33@gmail.com>
2020-08-19 12:29:18 -0400

    Create innerHTML.js

---
+++ DOM manipulation/innerHTML/innerHTML.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form');
+
+form.innerHTML;
+
+const ully = document.querySelector('ul');
+
+ully.innerHTML;
+
+form.innerHTML = 'yes this is text in the form now dfdsfdsfssdfasf!';
+
+form.innerHTML = '<b>I am a bold taggy!</b>';
+
+console.dir(form);
+
+const h1 = document.querySelector('h1');
+
+h1.innerHTML;
+
+h1.innerText;
+
+h1.innerHTML += ' is h1!!!';
+
+h1.innerText += '<b>is h1!!!!</b>';
