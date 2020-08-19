3301a1a9c89e21ebf31db24b7d703e7a6e2a49e2
Jackomonacko <jacksadowski33@gmail.com>
2020-08-19 10:59:52 -0400

    Create innerText and textContent.js

---
+++ DOM manipulation/innerText and textContent/innerText and textContent.js
@@ -0,0 +1,22 @@
+const h1 = document.querySelector('h1');
+
+h1.innerText;
+
+const ul1 = document.querySelector('ul');
+
+ul1.innerText;
+
+document.body('innerText')
+
+h1.innerText = "I'm  a mountain"
+
+ul1.innerText = "I am a big ul!!!"
+
+h1.textContent = "I am a big mountain"
+
+const p = document.querySelector('#main')
+
+p.innerText
+
+//sees script text and display non bold tag
+p.textContent
