02640d25ddd2d47e0310b8ef712686a2fe479340
Jackomonacko <jacksadowski33@gmail.com>
2020-08-22 16:05:30 -0400

    Create app.js

---
+++ The Very Basics/Hoisting/app.js
@@ -0,0 +1,25 @@
+console.log(fruit);
+var fruit = 'mango';
+console.log(fruit);
+
+// console.log(typeOfLobster);
+// let typeOfLobster = 'Hard Shell';
+// console.log(typeOfLobster);
+
+// console.log(typeOfLobster);
+// const typeOfLobster = 'Hard Shell';
+// console.log(typeOfLobster);
+
+bark();
+function bark() {
+	console.log('woof');
+}
+
+// console.log(yell);
+// var yell = function() {
+// 	console.log('ahhhhh');
+// };
+
+// yell();
+// let yell = function() {
+// 	conso
