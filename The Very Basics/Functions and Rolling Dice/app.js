b77b00adf411904967a979b03ae10392d14bcff1
Jackomonacko <jacksadowski33@gmail.com>
2020-08-22 16:04:21 -0400

    Create app.js

---
+++ The Very Basics/Functions and Rolling Dice/app.js
@@ -0,0 +1,19 @@
+function funkyFunkName() {
+	console.log(
+		"wow you did your first function! you're now a full stack dev!Make sure you use parenthesis to execute your function!"
+	);
+}
+
+function rollDaDie() {
+	let rolly = Math.floor(Math.random() * 6) + 1;
+	console.log(`Rolled number is: ${rolly}`);
+}
+
+function throwDaDice() {
+	rollDaDie();
+	rollDaDie();
+	rollDaDie();
+	rollDaDie();
+	rollDaDie();
+	rollDaDie();
+}
