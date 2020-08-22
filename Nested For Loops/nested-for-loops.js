0eaf7b147c9d73f9f6e8ab7b9b88b85876d61a86
Jackomonacko <jacksadowski33@gmail.com>
2020-08-22 16:14:50 -0400

    Delete nested-for-loops.js

--- Nested For Loops/nested-for-loops.js
+++
@@ -1,9 +0,0 @@
-const gameBoard = [ [ 6, 32, 8, 4 ], [ 8, 6, 5, 3 ], [ 4, 5, 8, 6 ], [ 3, 32, 8, 1 ] ];
-let totalScore = 0;
-for (let i = 0; i < gameBoard.length; i++) {
-	let row = gameBoard[i];
-	for (let j = 0; j < row.length; j++) {
-		totalScore += row[j];
-		console.log(totalScore);
-	}
-}
