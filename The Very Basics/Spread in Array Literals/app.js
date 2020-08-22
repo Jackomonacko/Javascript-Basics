1c3f715926d2a5c404bf42ab758afcfe0c1b9b63
Jackomonacko <jacksadowski33@gmail.com>
2020-08-22 16:50:03 -0400

    Create app.js

---
+++ The Very Basics/Spread in Array Literals/app.js
@@ -0,0 +1,14 @@
+const rifles = [ 'M1-22', 'M1-9', 'M1 Garand', 'M1903' ];
+
+const pistols = [ 'M11911', 'M1917', 'M1927', 'S&W Model 10' ];
+
+const subMachineGuns = [ 'M3A1', 'M1A1', 'M50' ];
+
+//const ww2Guns = [ 'M2', ...rifles, ...pistols ];
+
+const ww2Guns = [ ...rifles, ...pistols, ...subMachineGuns ];
+
+const riflesCopy = [ ...rifles ];
+
+//const rifleCopy2 = [ rifles ];
+//result it true when entered in console
