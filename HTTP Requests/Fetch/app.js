7a978f99e996ad4bdc8d9433e671bb1780fbe392
Jackomonacko <jacksadowski33@gmail.com>
2020-08-28 13:18:26 -0400

    Create app.js

---
+++ HTTP Requests/Fetch/app.js
@@ -0,0 +1,33 @@
+fetch('https://swapi.dev/api/planetss/')
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error(`Status code: ${response.status}`);
+		} else {
+			console.log(response);
+			response.json().then((data) => {
+				for (let planets of data.results) {
+					console.log(planets.name);
+				}
+			});
+		}
+	})
+	.catch((err) => {
+		console.log('uh ohh');
+		console.log(err);
+	});
+
+fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+	.then((response) => {
+		console.log(response);
+		response.json().then((btcData) => {
+			var objecto = btcData.bpi;
+			for (let priceData of Object.keys(objecto)) {
+				var currency = objecto[priceData];
+				console.log(currency.code, currency.description, currency.rate);
+			}
+		});
+	})
+	.catch((err) => {
+		console.log('uh ohh');
+		console.log(err);
+	});
