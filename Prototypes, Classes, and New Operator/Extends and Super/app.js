0551efd1027acb4a9f5459c2197e33e194c7f923
Jackomonacko <jacksadowski33@gmail.com>
2020-09-06 11:28:18 -0400

    Create app.js

---
+++ Prototypes, Classes, and New Operator/Extends and Super/app.js
@@ -0,0 +1,29 @@
+class Pet {
+	constructor(name, age) {
+		console.log(`In dog constructor`);
+		this.namee = name;
+		this.age = age;
+	}
+	eat() {
+		return `${this.namee} is eating`;
+	}
+}
+
+class Dog extends Pet {
+	constructor(name, age, timeLeft = 4) {
+		console.log(`In dog constructor, ${name}, ${age}, ${timeLeft}`);
+		super(name, age);
+		this.timeLeft = timeLeft;
+	}
+	woof() {
+		return 'bark wowoooof';
+	}
+}
+class Dog2 extends Pet {
+	howl() {
+		return 'hoooowwwwwwwwwwwwwwl';
+	}
+	eat() {
+		return `${this.namee} chows down!`;
+	}
+}
